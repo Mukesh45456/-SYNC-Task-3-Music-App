37f438f9336a16e65d8a8b682067bdb11e91e25f
Mukesh Nayak <132765988+Mukesh45456@users.noreply.github.com>
2023-05-29 10:48:44 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,44 @@
+let allMusic = [
+    {
+      name: "Lo Maan Liya",
+      artist: "Arijit Singh",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "AAJA NA FERRARI MEIN",
+      artist: "Armaan Malik",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Barsaat Ki Dhun",
+      artist: "Jubin Nautiyal",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Dil Ibaadat",
+      artist: "KK",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Pehli Dafa",
+      artist: "Atif Aslam",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Nakhre",
+      artist: "Zack Knight",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Gangster Paradise",
+      artist: "Coolio and Kylian Mash",
+      img: "music-16",
+      src: "music-16"
+    },
+];
